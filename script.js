5167b795922cafe2f6d6cd915d8ce9cda17ceb35
Vero <veroramirezmoreno@gmail.com>
2023-07-18 16:58:58 +0100

    Gofito can jump

--- script.js
+++ script.js
@@ -3,38 +3,45 @@
 const mainContainer = document.querySelector('main')
 const gofito = document.querySelector('#gofito')
 var impulso = 100;
-var gravedad = 2500;
+var gravedad = 0.5;
+
 
 //Cuidado modificar gofitoY si cambiamos el suelo
 var suelo = 170;
 var gofitoY = suelo;
 
+var jumping = false;
+var direction = 1;
+
+var timerJump;
+
 
 //Vamos a hacer que Gofito salte
 
 window.addEventListener('keydown', function (e) {
-    if (e.key == ' ') {
+    if (e.key === ' ' && !jumping) {
         console.log("Estoy saltando!!")
-        Jump()
+        jumping = true;
+        timerJump = setInterval(jump, 300);
     }
-})
+}
+)
 
-window.addEventListener('keyup', function (e) {
-    if (e.key == ' ') {
+function jump() {
+
+    if (gofitoY < suelo || gofitoY > 70) {
+        direction *= -1;
+    }
+    console.log(gofitoY);
+    gofitoY += impulso * direction;
+    gofito.style.top = gofitoY + 'px';
+
+    if (gofitoY >= suelo) {
+        clearInterval(timerJump)
+        jumping = false;
         console.log("Ya no estoy saltando")
-        gofitoY = suelo
     }
-})
 
-function Jump() {
-    let gofitoSaltando = gofitoY
-    
-    if (gofitoY === suelo) {
-        gofitoSaltando = gofitoY - impulso
-
-    }
-    gofito.style.top = gofitoSaltando +"px"
 
 }
-
 
