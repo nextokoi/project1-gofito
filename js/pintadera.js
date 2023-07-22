4f6e3afffa11604871eecd721254bc3243799763
Vero <veroramirezmoreno@gmail.com>
2023-07-22 15:51:02 +0100

    Comments in pintadera.js

--- js/pintadera.js
+++ js/pintadera.js
@@ -1,27 +1,27 @@
-function Pintadera(x, y, parent, gofito) {
-    let self = this
+function Pintadera(x, y, parent, gofito) { //We create the constructor function of Pintadera
+    let self = this //First of all...
     this.x = x
     this.y = y
     this.width = 74
     this.height = 74
     this.speed = 18
-    this.sprite = document.createElement('div')
+    this.sprite = document.createElement('div') //This element represents the 'pintadera' in the DOM
 
-    this.insertPintadera = function () {
+    this.insertPintadera = function () { //Function to inster the 'pintadera'
         self.sprite.classList.add('pintadera')
         self.sprite.style.left = self.x + "px"
         self.sprite.style.top = self.y + "px"
         parent.appendChild(self.sprite)
     }
 
-    this.movePintadera = function () {
+    this.movePintadera = function () { //Function to move it, just like obstacles
         self.checkCollisionWin()
         self.x -= self.speed
         self.sprite.style.left = self.x + "px";
 
     }
 
-    this.checkCollisionWin = function () {
+    this.checkCollisionWin = function () { //Function to check collisions, just like obstacles
         if (self.x <= gofito.x + gofito.width &&
             self.x + self.width >= gofito.x &&
             self.y <= gofito.y + gofito.height &&
@@ -33,7 +33,7 @@ function Pintadera(x, y, parent, gofito) {
             // screen4.setAttribute('class', 'wrapper')
         }
     }
-    self.timerId = setInterval(self.movePintadera, 100);
+    self.timerId = setInterval(self.movePintadera, 100); //Interval to move the 'pintadera'
 
 }
 
